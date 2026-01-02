f480004d97fc4fb1c299c7bd9003a367adf9729d
Master Shubham <ommylight@gmail.com>
2026-01-02 15:54:23 +0530

    Improve code and Error handling

--- README.md
+++ README.md
@@ -2,5 +2,17 @@
 #i can create using HTML, CSS, JavaScrip.<br>
 #This is Responsive Weather App.<br>
 
+
+![Page 1:Teasting perpose](image.png)
+
+![Page 2: Error handling](image-1.png)
+
+
+
+
+
+
+
 ⚠️ This project uses a free OpenWeather API key for learning purposes.
 For production use, API keys should be handled via a backend.
+
--- script/script.js
+++ script/script.js
@@ -1,54 +1,51 @@
 // NOTE: This API key is for demo/learning purposes only
-const apiKey = '3f986598a2dec8a39bd934bdfa79d04c';
+const apiKey = "3f986598a2dec8a39bd934bdfa79d04c";
 
-const apiUrl = "https://api.openweathermap.org/data/2.5/weather?units=metric&q=";
-const searchInput = document.querySelector('.search input');
-const searchBtn = document.querySelector('.search button');
-const weatherIcon = document.querySelector('.weather-icon');
-const errorMsg = document.querySelector('.errorMsg');
+const apiUrl =
+  "https://api.openweathermap.org/data/2.5/weather?units=metric&q=";
+const searchInput = document.querySelector(".search input");
+const searchBtn = document.querySelector(".search button");
+const weatherIcon = document.querySelector(".weather-icon");
+const errorMsg = document.querySelector(".errorMsg");
 
 async function checkWheathe(city) {
-    const response = await fetch(apiUrl + city + `&appid=${apiKey}`);
-
-    if (response.status === 404) {
-        document.querySelector('.errorMsg').style.display = "block"
-        document.querySelector('.weather').style.display = "none"
-    } else {
-
-        try {
-            const data = await response.json();
-        document.querySelector('.temp').innerHTML = Math.round(data.main.temp) + "°C";
-        document.querySelector('.city').innerHTML = data.name;
-        document.querySelector('.humidity').innerHTML = data.main.humidity + '%';
-        document.querySelector('.wind').innerHTML = data.wind.speed + 'km/h';
-
-        if (data.weather[0].main == "Clouds") {
-            weatherIcon.src = "/images/clouds.png";
-        }
-        else if (data.weather[0].main == "Rain") {
-            weatherIcon.src = "/images/rain.png";
-
-        } else if (data.weather[0].main == "Drizzle") {
-            weatherIcon.src = "/images/drizzle.png";
-
-        } else if (data.weather[0].main == "Mist") {
-            weatherIcon.src = "/images/mist.png";
-
-        } else if (data.weather[0].main == "Clear") {
-            weatherIcon.src = "/images/clear.png";
-        }
-
-        document.querySelector('.weather').style.display = "block"
-        document.querySelector('.errorMsg').style.display = "none"
+  const response = await fetch(apiUrl + city + `&appid=${apiKey}`);
 
 
-        } catch (error) {
-            console.log(error);   
-        }
+  if (!response.ok ||searchInput === "" || isNaN(searchInput)) {
+    errorMsg.style.display = "block";
+    weatherIcon.style.display = "none";
+    searchInput.value="";
+    
+  } else {
+    try {
+      const data = await response.json();
+      document.querySelector(".temp").innerHTML =
+        Math.round(data.main.temp) + "°C";
+      document.querySelector(".city").innerHTML = data.name;
+      document.querySelector(".humidity").innerHTML = data.main.humidity + "%";
+      document.querySelector(".wind").innerHTML = data.wind.speed + "km/h";
+
+      if (data.weather[0].main == "Clouds") {
+        weatherIcon.src = "images/clouds.png";
+      } else if (data.weather[0].main == "Rain") {
+        weatherIcon.src = "images/rain.png";
+      } else if (data.weather[0].main == "Drizzle") {
+        weatherIcon.src = "images/drizzle.png";
+      } else if (data.weather[0].main == "Mist") {
+        weatherIcon.src = "images/mist.png";
+      } else if (data.weather[0].main == "Clear") {
+        weatherIcon.src = "images/clear.png";
+      }
+
+      document.querySelector(".weather").style.display = "block";
+      document.querySelector(".errorMsg").style.display = "none";
+    } catch (error) {
+      console.log(error);
     }
+  }
 }
 
-searchBtn.addEventListener('click', () => {
-    checkWheathe(searchInput.value);
-})
-
+searchBtn.addEventListener("click", () => {
+  checkWheathe(searchInput.value);
+});
